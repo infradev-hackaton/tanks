fc9bf3fcaeeb8b0189f1f874ccbd6ec1ceb7727d
Dmitrii Golyshev <spacy@yandex-team.ru>
2016-06-11 00:15:21 +0300

    Little refactor

--- index.js
+++ index.js
@@ -1,139 +1,158 @@
-var SIZE = 150;
-var game = new TheGame({
-    size: SIZE,
-    unitsCount: 5
-});
+var GAME = (function () {
+    var SIZE = 150;
+    var game = new TheGame({
+        size: SIZE,
+        unitsCount: 5
+    });
 
-var fpsAvg = 0;
-var framesCount = 0;
-var start = new Date();
-function renderGame(game) {
-    var v = new Array(SIZE);
-    var now = new Date();
-    var dur = now - start;
-    framesCount += 1;
+    var fpsAvg = 0;
+    var framesCount = 0;
+    var start = new Date();
 
-    if (dur > 1000) {
-        fpsAvg += framesCount;
-        fpsAvg /= 2;
-        framesCount = 0;
-        start = now - (dur - 1000);
-    }
+    function renderGame(game) {
+        var v = new Array(SIZE);
+        var now = new Date();
+        var dur = now - start;
+        framesCount += 1;
 
-    game.each(function (unit, i) {
-        if (unit instanceof Protagonist) {
-            if (unit.getDirection()) {
-                v[i] = '\ud83d\ude0e\ud83d\udc49';
-            } else {
-                v[i] = '\ud83d\udc48\ud83d\ude0e';
-            }
-        } else if (unit instanceof Antagonist5) {
-            v[i] = '\ud83d\udc80';
-        } else if (unit instanceof Antagonist4) {
-            v[i] = '\ud83d\ude08';
-        } else if (unit instanceof Antagonist3) {
-            v[i] = '\ud83d\ude21';
-        } else if (unit instanceof Antagonist2) {
-            v[i] = '\ud83d\ude01';
-        } else if (unit instanceof Antagonist1) {
-            v[i] = '\ud83d\ude2c';
-        } else if (unit instanceof Bullet) {
-            if (unit.getDirection()) {
-                v[i] = '\u204d';
-            } else {
-                v[i] = '\u204c';
-            }
-        } else {
-            v[i] = ' ';
+        if (dur > 1000) {
+            fpsAvg += framesCount;
+            fpsAvg /= 2;
+            framesCount = 0;
+            start = now - (dur - 1000);
         }
-    });
 
-    window.location.replace('#' + v.join('') + ' FPS: ' + fpsAvg.toFixed(2));
+        game.each(function (unit, i) {
+            if (unit instanceof Protagonist) {
+                if (unit.getDirection()) {
+                    v[i] = '\ud83d\ude0e\ud83d\udc49';
+                } else {
+                    v[i] = '\ud83d\udc48\ud83d\ude0e';
+                }
+            } else if (unit instanceof Antagonist5) {
+                v[i] = '\ud83d\udc80';
+            } else if (unit instanceof Antagonist4) {
+                v[i] = '\ud83d\ude08';
+            } else if (unit instanceof Antagonist3) {
+                v[i] = '\ud83d\ude21';
+            } else if (unit instanceof Antagonist2) {
+                v[i] = '\ud83d\ude01';
+            } else if (unit instanceof Antagonist1) {
+                v[i] = '\ud83d\ude2c';
+            } else if (unit instanceof Bullet) {
+                if (unit.getDirection()) {
+                    v[i] = '\u204d';
+                } else {
+                    v[i] = '\u204c';
+                }
+            } else {
+                v[i] = ' ';
+            }
+        });
 
-    return Boolean(game.getHero());
-}
+        window.location.replace('#' + v.join('') + ' FPS: ' + fpsAvg.toFixed(2));
 
-renderGame(game);
-
-function onArrowKd(e) {
-    if (e.key === 'ArrowRight') {
-        $(window)
-            .off('keydown', onArrowKd)
-            .on('keyup', onArrowKu);
-        game.getHero().
-        setDirection(true).
-        startMoving();
-    } else if (e.key === 'ArrowLeft') {
-        $(window)
-            .off('keydown', onArrowKd)
-            .on('keyup', onArrowKu);
-        game.getHero().
-        setDirection(false).
-        startMoving();
-    }
-}
-
-function onArrowKu(e) {
-    if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
-        game.getHero().stopMoving();
-        $(window)
-            .off('keyup', onArrowKu)
-            .on('keydown', onArrowKd);
-    }
-}
-
-function onSpaceKd(e) {
-    if (e.key === ' ') {
-        $(window)
-            .off('keydown', onSpaceKd)
-            .on('keyup', onSpaceKu);
-        game.getHero().fire();
-    }
-}
-
-function onSpaceKu(e) {
-    if (e.key === ' ') {
-        $(window)
-            .on('keydown', onSpaceKd)
-            .off('keyup', onSpaceKu);
-    }
-}
-
-function doLayout() {
-    if (game.isPaused()) {
-        $(window)
-            .off('keydown', onArrowKd)
-            .off('keyup', onArrowKu)
-            .off('keydown', onSpaceKd)
-            .off('keyup', onSpaceKu);
-
-        return;
+        return Boolean(game.getHero());
     }
 
-    if (!renderGame(game)) {
-        game.stop();
-    }
+    renderGame(game);
 
-    TheGame.pushFrame(doLayout);
-}
-
-$(function () {
-    $('.button_action_reset').click(function () {
-        game.init();
-        renderGame(game);
-    });
-
-    $('.button_action_start').click(function () {
-        if (game.start()) {
+    function onArrowKd(e) {
+        if (e.key === 'ArrowRight') {
             $(window)
-                .on('keydown', onArrowKd)
-                .on('keydown', onSpaceKd);
-
-            doLayout();
+                .off('keydown', onArrowKd)
+                .on('keyup', onArrowKu);
+            game.getHero().
+            setDirection(true).
+            startMoving();
+        } else if (e.key === 'ArrowLeft') {
+            $(window)
+                .off('keydown', onArrowKd)
+                .on('keyup', onArrowKu);
+            game.getHero().
+            setDirection(false).
+            startMoving();
         }
+    }
+
+    function onArrowKu(e) {
+        if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+            game.getHero().stopMoving();
+            $(window)
+                .off('keyup', onArrowKu)
+                .on('keydown', onArrowKd);
+        }
+    }
+
+    function onSpaceKd(e) {
+        if (e.key === ' ') {
+            $(window)
+                .off('keydown', onSpaceKd)
+                .on('keyup', onSpaceKu);
+            game.getHero().fire();
+        }
+    }
+
+    function onSpaceKu(e) {
+        if (e.key === ' ') {
+            $(window)
+                .on('keydown', onSpaceKd)
+                .off('keyup', onSpaceKu);
+        }
+    }
+
+    function doLayout() {
+        if (game.isPaused()) {
+            $(window)
+                .off('keydown', onArrowKd)
+                .off('keyup', onArrowKu)
+                .off('keydown', onSpaceKd)
+                .off('keyup', onSpaceKu);
+
+            return;
+        }
+
+        if (!renderGame(game)) {
+            game.stop();
+            $('.page')
+                .addClass('page_pause');
+        }
+
+        TheGame.pushFrame(doLayout);
+    }
+
+    $(function () {
+        $('.button_action_reset').click(function () {
+            $('.page')
+                .removeClass('page_playing')
+                .addClass('page_pause');
+
+            game.init();
+            renderGame(game);
+        });
+
+        $('.button_action_start').click(function () {
+            if (game.start()) {
+                $(window)
+                    .on('keydown', onArrowKd)
+                    .on('keydown', onSpaceKd);
+
+                doLayout();
+            }
+
+            $('.page')
+                .removeClass('page_pause')
+                .addClass('page_playing');
+        });
+
+        $('.button_action_pause').click(function () {
+            game.stop();
+
+            $('.page')
+                .removeClass('page_playing')
+                .addClass('page_pause');
+        });
     });
 
-    $('.button_action_pause').click(function () {
-        game.stop();
-    });
-});
+    return game;
+})();
